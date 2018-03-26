026d22d84cec2610af0bb439c9cce651d7a668b7
Saeed Alalwan <salalwan@eng.mach7.local>
2018-03-25 22:29:40 -0400

    Center login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,19 +14,20 @@ const styles = theme => ({
     "grid-template-areas": `"a a a a b b b b"
     "a a a a b b b b"
     "c c c c d d d d"
-    "c c c c d d d d"`,
+    "c c c c d d d d"`
   },
-  form: {
+  paper: {
     "grid-area": "c",
     "justify-self": "center"
-
   },
   textField: {
     margin: theme.spacing.unit,
-    width: 300
+    width: "300px"
   },
   button: {
-    margin: theme.spacing.unit
+    margin: theme.spacing.unit,
+    width: "50%",
+    "justify-content": "center"
   }
 });
 
@@ -40,21 +41,37 @@ export default withStyles(styles)(
     classes
   }) => (
     <div className={classes.root}>
-      <FormControl onSubmit={e => e.preventDefault()} className={classes.form}>
-        <TextField
-          label="Username"
-          type={"text"}
-          onChange={({ target }) => update_username({ username: target.value })}
-        />
-        <TextField
-          label="Password"
-          type={"password"}
-          onChange={({ target }) => update_password({ password: target.value })}
-        />
-        <Button variant="raised" color="primary" className={classes.button} onClick={login}>
-          LOG IN
-        </Button>
-      </FormControl>
+      <Paper className={classes.paper} elevation={4}>
+        <FormControl onSubmit={e => e.preventDefault()}>
+          <TextField
+            label="Username"
+            type={"text"}
+            className={classes.textField}
+            onChange={({ target }) =>
+              update_username({ username: target.value })
+            }
+          />
+          <TextField
+            label="Password"
+            type={"password"}
+            className={classes.textField}
+            onChange={({ target }) =>
+              update_password({ password: target.value })
+            }
+          />
+          <div align="center">
+            <Button
+              align="center"
+              variant="raised"
+              color="primary"
+              className={classes.button}
+              onClick={login}
+            >
+              LOG IN
+            </Button>
+          </div>
+        </FormControl>
+      </Paper>
     </div>
   )
 );
